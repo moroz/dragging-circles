28f938a219c5165b0adf03d800ac1bbd5cb22443
Karol Moroz <karol@moroz.dev>
2022-07-27 16:44:29 +0200

    Add a canvas with clickable circles

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useCallback, useRef } from "react";
+import "./App.css";
+
+const ASPECT_RATIO = 16 / 9;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = Math.ceil(CANVAS_WIDTH / ASPECT_RATIO);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  const getMousePosition = useCallback(
+    (e: React.MouseEvent) => {
+      const ctm = svgRef.current!.getScreenCTM()!;
+      return {
+        x: (e.clientX - ctm.e) / ctm.a,
+        y: (e.clientY - ctm.f) / ctm.d
+      };
+    },
+    [svgRef]
+  );
+
+  const onClick = useCallback((e: React.MouseEvent) => {
+    const coords = getMousePosition(e);
+    console.log(coords);
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <svg
+        ref={svgRef}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox={`0 0 ${CANVAS_WIDTH} ${CANVAS_HEIGHT}`}
+        id="canvas"
+        width={CANVAS_WIDTH}
+        height={CANVAS_HEIGHT}
+      >
+        <circle cx="50" cy="50" r="25" className="circle" onClick={onClick} />
+        <circle cx="130" cy="80" r="25" className="circle" onClick={onClick} />
+      </svg>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
