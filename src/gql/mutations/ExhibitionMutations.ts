d5dab7de868569b8b20b72fa208156cd0e034fea
Karol Moroz <karol@moroz.dev>
2022-09-03 23:34:53 +0200

    Add mutation to update exhibition data

--- src/gql/mutations/ExhibitionMutations.ts
+++ src/gql/mutations/ExhibitionMutations.ts
@@ -5,6 +5,8 @@ import {
   ArtworkShapeInput
 } from "../../interfaces/artwork";
 import { ID, MutationResult } from "../../interfaces/common";
+import { Exhibition } from "../../interfaces/exhibitions";
+import { GRAPHQL_API_URI } from "../client";
 
 export const SAVE_ARTWORK_LAYOUT_MUTATION = gql`
   mutation SaveLayout($exhibitionId: ID!, $shapes: [ArtworkShapeInput!]!) {
@@ -27,7 +29,7 @@ export const SAVE_ARTWORK_LAYOUT_MUTATION = gql`
 `;
 
 export interface SaveArtworkLayoutMutationResult {
-  result: MutationResult<Artwork>;
+  result: MutationResult<Exhibition>;
 }
 
 export interface SaveArtworkLayoutMutationVariables {
@@ -73,3 +75,54 @@ export const useCreateArtworkMutation = () =>
   useMutation<CreateArtworkMutationResult, CreateArtworkMutationVariables>(
     CREATE_ARTWORK_MUTATION
   );
+
+export const UPDATE_EXHIBITION_MUTATION = gql`
+  mutation UpdateExhibition($params: UpdateExhibitionParams!) {
+    result: updateExhibition(params: $params) {
+      success
+      errors {
+        key
+        message
+      }
+      data {
+        id
+        title
+        background
+      }
+    }
+  }
+`;
+
+export interface UpdateExhibitionParams {
+  title?: string;
+  background?: File;
+}
+
+export interface UpdateExhibitionMutationResult {
+  result: MutationResult<Exhibition>;
+}
+
+export const updateExhibitionMutation = ({
+  title,
+  background
+}: UpdateExhibitionParams) => {
+  return new Promise((resolve, reject) => {
+    const data = new FormData();
+    data.append("query", UPDATE_EXHIBITION_MUTATION.loc!.source.body);
+    data.append(
+      "variables",
+      JSON.stringify({ params: { title, background: background && "image" } })
+    );
+    if (background) {
+      data.append("image", background);
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.open("POST", GRAPHQL_API_URI, true);
+    xhr.addEventListener("load", () => {
+      resolve(JSON.parse(xhr.response));
+    });
+    xhr.addEventListener("error", reject);
+    xhr.send(data);
+  });
+};
