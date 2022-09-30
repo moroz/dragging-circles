1ffae5b0db130256547afb55612acf3044b04304
Karol Moroz <k.j.moroz@gmail.com>
2022-09-30 23:05:11 +0200

    Display exhibition title on canvas

--- src/components/ArtworkCanvas/index.tsx
+++ src/components/ArtworkCanvas/index.tsx
@@ -7,10 +7,34 @@ import styles from "./Canvas.module.sass";
 import Layout from "../../layout";
 import Shape from "./Shape";
 import { LayoutLoader } from "../../layout/Loader";
+import clsx from "clsx";
 
 const ASPECT_RATIO = 16 / 9;
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = Math.ceil(CANVAS_WIDTH / ASPECT_RATIO);
+
+//   def title_position(1), do: {400, 30}
+//   def title_position(2), do: {800 - 18, 30}
+//   def title_position(3), do: {18, 225}
+//   def title_position(4), do: {400, 225}
+//   def title_position(5), do: {800 - 18, 225}
+//   def title_position(6), do: {18, 450 - 30}
+//   def title_position(7), do: {400, 450 - 30}
+//   def title_position(8), do: {800 - 18, 450 - 30}
+//   def title_position(_), do: {18, 30}
+// end
+
+const COORDS = [
+  [18, 30],
+  [400, 30],
+  [800 - 18, 30],
+  [18, 225],
+  [400, 225],
+  [800 - 18, 225],
+  [18, 450 - 30],
+  [400, 450 - 30],
+  [800 - 18, 450 - 30]
+];
 
 function ArtworkCanvas() {
   const { data, loading } = useGetExhibitionQuery();
@@ -25,6 +49,15 @@ function ArtworkCanvas() {
   }, [data, reset]);
 
   useEffect(onReset, [loading, data]);
+
+  const [x, y] = exhibition?.titlePosition
+    ? COORDS[exhibition.titlePosition]
+    : [18, 30];
+
+  const cssVars = {
+    "--border-color": exhibition?.borderColor ?? undefined,
+    "--font-color": exhibition?.fontColor ?? undefined
+  } as React.CSSProperties;
 
   if (loading) return <LayoutLoader />;
 
@@ -41,6 +74,7 @@ function ArtworkCanvas() {
           onMouseMove={onMouseMove}
           onClick={onClick}
           onMouseLeave={onMouseLeave}
+          style={cssVars}
         >
           {exhibition?.background ? (
             <image href={exhibition.background} width={800} />
@@ -55,6 +89,18 @@ function ArtworkCanvas() {
               onMouseUp={onDragEnd}
             />
           ))}
+          {exhibition?.showTitle ? (
+            <text
+              x={x}
+              y={y}
+              className={clsx(
+                styles.title,
+                styles[`position-${exhibition.titlePosition ?? 0}`]
+              )}
+            >
+              {exhibition.title}
+            </text>
+          ) : null}
         </svg>
         <Toolbar onReset={onReset} />
       </div>
